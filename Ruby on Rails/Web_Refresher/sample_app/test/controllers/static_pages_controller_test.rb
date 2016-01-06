6598c62fa24020fcb5216dc543f2f03f947afe47
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2016-01-06 00:34:33 -0800

    Add a Contact page (solve exercise 3.2)

--- Ruby on Rails/Web_Refresher/sample_app/test/controllers/static_pages_controller_test.rb
+++ Ruby on Rails/Web_Refresher/sample_app/test/controllers/static_pages_controller_test.rb
@@ -24,4 +24,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About #{@title_end}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact #{@title_end}"
+  end
+
 end
