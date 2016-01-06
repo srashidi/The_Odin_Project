47f7b0b57cfcb9cb260b35e696cbbf927bb9a4f8
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2016-01-06 00:31:11 -0800

    Eliminate repetition (solve exercise 3.1)

--- Ruby on Rails/Web_Refresher/sample_app/test/controllers/static_pages_controller_test.rb
+++ Ruby on Rails/Web_Refresher/sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+	def setup
+		@title_end = "| Ruby on Rails Tutorial Sample App"
+	end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home #{@title_end}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help #{@title_end}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About #{@title_end}"
   end
 
 end
